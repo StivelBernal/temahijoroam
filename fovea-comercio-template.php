6ad31de116b2b75497a74e785336b353b06072a8
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-06-14 23:02:54 -0500

    update

--- fovea-comercio-template.php
+++ fovea-comercio-template.php
@@ -11,33 +11,36 @@ if (have_posts()) : while (have_posts()) : the_post();
 
         //Get classes for holder and holder inner
         $mkdf_holder_params = roam_mikado_get_holder_params_blog();
+
+        /**Get user meta mapa, galeria, comentarios, servicios */
+
 ?>
 
-        <div class="fovea-comercio-single">
+        <div class="fovea-comercio-single" ng-app="comercio_page" ng-controller="pageController">
 
             <div class="fovea-nav-holder">
 
                 <div class="mkdf-grid row-wrap">
                    
-                        <div class="s-flex comercio-tabs" ng-click="tab === 1">
+                        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 1  }" ng-click="tab = 1">
                             <span class="mkdf-tour-nav-section-icon dripicons-document"></span>
                             <span class="mkdf-tour-nav-section-title">
                                 <?php echo _x('Información', 'nav menu single page comercio', 'roam-child');  ?> </span>
                         </div>
 
-                        <div class="s-flex comercio-tabs" ng-click="tab === 2">
+                        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 2 }" ng-click="tab = 2">
                             <span class="mkdf-tour-nav-section-icon dripicons-map"></span>
                             <span class="mkdf-tour-nav-section-title">
                             <?php echo _x('Servicios', 'nav menu single page comercio', 'roam-child');  ?> </span>
                         </div>
                       
-                        <div class="s-flex comercio-tabs" ng-click="tab === 3">
+                        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 3 }" ng-click="tab = 3">
                             <span class="mkdf-tour-nav-section-icon dripicons-location"></span>
                             <span class="mkdf-tour-nav-section-title">
                             <?php echo _x('Ubicación', 'nav menu single page comercio', 'roam-child');  ?> </span>
                         </div>
                     
-                        <div class="s-flex comercio-tabs" ng-click="tab === 4">
+                        <div class="s-flex comercio-tabs" ng-class="{'active-item': tab === 4 }" ng-click="tab = 4">
                             <span class="mkdf-tour-nav-section-icon dripicons-camera"></span>
                             <span class="mkdf-tour-nav-section-title">
                             <?php echo _x('Comentarios', 'nav menu single page comercio', 'roam-child');  ?> </span>
@@ -54,8 +57,36 @@ if (have_posts()) : while (have_posts()) : the_post();
                 <div class="fovea-container-inner mkdf-container-inner">
                     <div class="clearfix mkdf-grid-medium-gutter">
                         <div class="mkdf-grid-col-9">
-                            <article class="mkdf-tour-item-wrapper">
-                                fdgfg
+                            <article ng-hide="tab !== 1" class="mkdf-tour-item-wrapper">
+                               <div class="fovea-content-comercio">
+                                   <?php  the_content(); ?>
+                                <div>
+
+                            </article>
+                            <article ng-hide="tab !== 2"class="mkdf-tour-item-wrapper">
+                            
+                                <div class="fovea-content-servicios">
+                                    SERVICIOS
+                                <div>
+
+                            </article>
+
+                            <article ng-hide="tab !== 3"class="mkdf-tour-item-wrapper">
+                            
+
+                                <div  class="fovea-content-ubicacion">
+                                    UBICACION
+                                <div>
+
+                            </article>
+
+                            <article ng-hide="tab !== 4"class="mkdf-tour-item-wrapper">
+                                <div  class="fovea-content-comentarios">
+                                      <?php if (comments_open()){
+                                            comments_template();
+                                        }
+                                ?>
+                                <div>
                             </article>
                         </div>
 
