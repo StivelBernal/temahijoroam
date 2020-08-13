6596d3a0d4bdcc0e205673f58c5a9cd5d6215073
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-13 15:17:44 -0500

    dfgdfg

--- single.php
+++ single.php
@@ -9,14 +9,14 @@ $tipos_alcaldia = ['emergencias', 'transporte' ];
 if(in_array($terms[0]->slug, $tipos_alcaldia)){
 
 	get_template_part( 'fovea-gobierno-template' );
+	/**solo tres la ubicacion las (redes-contenido) comentarios*/
 
 }else{
 	get_template_part( 'fovea-comercio-template' );
 }
+var_dump($post->author);
+do_shortcode('[ser_like_share]');
 
-do_shortcode('ser_like_share');
-
-//var_dump($post);
 
 get_footer(); 
 
