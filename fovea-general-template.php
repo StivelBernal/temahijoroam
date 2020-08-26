6c4908e8ec07088f28f5427cb78aec1e903d3f71
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-25 22:29:59 -0500

    dfsdf

--- fovea-general-template.php
+++ fovea-general-template.php
@@ -19,11 +19,19 @@ if (have_posts()) : while (have_posts()) : the_post();
 
         //Action added for applying module specific filters that couldn't be applied on init
         do_action('roam_mikado_blog_single_loaded');
+        
+        if (has_post_thumbnail()) {
+
+            $imagen_destacada = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0];
+        } else {
+
+            $imagen_destacada = 'https://golfodemorrosquillo.com/wp-content/uploads/2020/08/AZUL-OSCURO-con-logo-Horizontal.png';
+        }
 
         //Get classes for holder and holder inner
         $mkdf_holder_params = roam_mikado_get_holder_params_blog();
         echo '
-         <div class="s-featured-header" style="background-image: url('.get_the_post_thumbnail_url(get_the_ID(),'full').')"></div>
+         <div class="s-featured-header" style="background-image: url('. $imagen_destacada.')"></div>
         ';
 ?>
        
--- templates-fovea/categoria-sitios.php
+++ templates-fovea/categoria-sitios.php
@@ -50,15 +50,15 @@ if( have_posts() ){
            
            <?php
 
-                if( has_post_thumbnail() ){
-                            
-                    $imagen_destacada = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
+               
+                if (has_post_thumbnail()) {
 
-                }else{
+                    $imagen_destacada = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0];
+                } else {
 
-                    $imagen_destacada = '/wp-content/plugins/ser_lib/assets/img/images.png';
-
+                    $imagen_destacada = 'https://golfodemorrosquillo.com/wp-content/uploads/2020/08/AZUL-OSCURO-con-logo-Horizontal.png';
                 }
+
             ?> 
 
             <div class="fovea-category-1">
