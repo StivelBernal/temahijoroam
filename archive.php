28b7b3e67033879d78c35e4209596eb7d4314cc6
Brayan Stivel Bernal Garcia <brayan.bernalg@Gmail.com>
2020-08-28 17:41:54 -0500

    dsf

--- archive.php
+++ archive.php
@@ -76,7 +76,7 @@ if(!isset($_GET["busqueda"])){
 		if(!isset($_GET["busqueda"])){				
 			$id_municipio = get_post_meta( $value[0]->post_id, 'alcaldiau');
 			$id_gobernacion = get_post_meta( $value[0]->post_id, 'gobernacion');
-			var_dump($id_municipio);
+			var_dump($id_municipio[0] == '');
 			if(isset($id_municipio[0]) || $id_municipio[0] !== ''){
 				echo '<script> var id_municipio = '.$id_municipio[0].';</script>';
 			}
